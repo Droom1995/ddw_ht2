dd2476108afa08020fbb90168cf659ff29175076
Droom1995 <husiev95@gmail.com>
2017-03-26 22:32:28 +0200

    Csvs added

--- main.py
+++ main.py
@@ -7,14 +7,28 @@ import numpy as np
 import pandas as pd
 
 
-def calc_measures(matrix, similarity_function, ref_set, n):
-    sim = np.array(similarity_function(matrix[n - 1], matrix[0:(n - 1)])[0])
-    topRelevant = set(sim.argsort()[-len(relevant_set):][::-1] + 1)
+def calc_cosine_measures(matrix, ref_set, n):
+    sim = np.array(cosine_similarity(matrix[n - 1], matrix[0:(n - 1)])[0])
+    topRelevant = set(sim.argsort()[-10:][::-1] + 1)
     intersec = topRelevant & ref_set
     precision = len(intersec) / len(topRelevant)
     recall = len(intersec) / len(ref_set)
     f_measure = 0
-    if len(intersec)==0:
+    if len(intersec) == 0:
+        f_measure = 0
+    else:
+        f_measure = 2 * (precision * recall) / (precision + recall)
+    return [precision, recall, f_measure]
+
+
+def calc_euclidean_measures(matrix, ref_set, n):
+    sim = np.array(euclidean_distances(matrix[n - 1], matrix[0:(n - 1)])[0])
+    topRelevant = set(sim.argsort()[:10][::-1] + 1)
+    intersec = topRelevant & ref_set
+    precision = len(intersec) / len(topRelevant)
+    recall = len(intersec) / len(ref_set)
+    f_measure = 0
+    if len(intersec) == 0:
         f_measure = 0
     else:
         f_measure = 2 * (precision * recall) / (precision + recall)
@@ -41,8 +55,12 @@ for q in range(225):
     document = set(document)
     relevant_documents.append(document)
 
-res_df = pd.DataFrame(columns=["Query", "Precision", "Recall", "F-measure", "Method"])
-for i in range(0, 20):
+df_arr = []
+for i in range(0, 6):
+    df_arr.append(pd.DataFrame(columns=["Query", "Precision", "Recall", "F-measure"]))
+df_names = ["Bin-Euclid","Bin-Cosine","TF-Euclid", "TF-Cosine", "TFIDF-Euclid", "TFIDF-Cosine"]
+
+for i in range(0, 2):
     query = queries[i]
     corpus = ref_corpus[:]
     relevant_set = relevant_documents[i]
@@ -50,21 +68,22 @@ for i in range(0, 20):
 
     # init pure TF
     count_vectorizer = CountVectorizer()
+    binary_vectorizer = CountVectorizer(binary=True)
 
     tf_matrix = count_vectorizer.fit_transform(corpus)
-
+    bin_matrix = binary_vectorizer.fit_transform(corpus)
     # init TF-IDF vectorizer
     tfidf_vectorizer = TfidfVectorizer()
 
     # prepare matrix
     tfidf_matrix = tfidf_vectorizer.fit_transform(corpus)
 
-    similarity_functions = [cosine_similarity, euclidean_distances]
-    matrices = [tf_matrix, tfidf_matrix]
-    for matrix in matrices:
-        for func in similarity_functions:
-            res_df.loc[len(res_df)] = [i] + calc_measures(matrix, func, relevant_set, len(corpus)) + ["TF-IDF/Cosine"]
+    df_arr[0].loc[len(df_arr[0])] = [i] + calc_euclidean_measures(bin_matrix, relevant_set, len(corpus))
+    df_arr[1].loc[len(df_arr[1])] = [i] + calc_cosine_measures(bin_matrix, relevant_set, len(corpus))
+    df_arr[2].loc[len(df_arr[2])] = [i] + calc_euclidean_measures(tf_matrix, relevant_set, len(corpus))
+    df_arr[3].loc[len(df_arr[3])] = [i] + calc_cosine_measures(tf_matrix, relevant_set, len(corpus))
+    df_arr[4].loc[len(df_arr[4])] = [i] + calc_euclidean_measures(tfidf_matrix, relevant_set, len(corpus))
+    df_arr[5].loc[len(df_arr[5])] = [i] + calc_cosine_measures(tfidf_matrix, relevant_set, len(corpus))
 
-print(res_df)
-
-res_df.to_csv("out.csv", sep=',')
+for i in range(0,len(df_arr)):
+    df_arr[i].to_csv(df_names[i] + ".csv", sep=',')
